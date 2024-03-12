471e019ece353f57bb626a0fc89fcef96a2b1073
a.kanatbek <aman.kanatbek@chefmarket.ru>
2024-03-12 19:31:28 +0600

    feature 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     androidTestImplementation(libs.androidx.junit)
