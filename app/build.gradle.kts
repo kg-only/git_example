4a684dbf6dc963baf8dfef5b3927e6ab5b9040ad
a.kanatbek <aman.kanatbek@chefmarket.ru>
2024-03-12 19:31:03 +0600

    feature 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
